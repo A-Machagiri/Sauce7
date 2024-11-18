8f8ee55a609072f1749c9e9f0f63a8756f2d73aa
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-19 00:18:22 +0530

    Create PageObjects/LoginPage_Locators.cs

---
+++ PageObjects/LoginPage_Locators.cs
@@ -0,0 +1,6 @@
+public partial class LoginPage
+{
+    public string UsernameField => "input#username";
+    public string PasswordField => "input#password";
+    public string LoginButton => "button#login";
+}
